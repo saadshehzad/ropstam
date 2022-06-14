9ec481c535e292455d493ce5fc06c3ef6e075db6
Muhammad Shehzad <xaadxhehzad@gmail.com>
2022-06-14 08:44:26 +0000

    Customize admin for product app

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created', 'updated', 'name')
+    list_filter = ('created', 'updated')
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created',
+        'updated',
+        'category',
+        'color',
+        'model',
+        'reg_no',
+    )
+    list_filter = ('created', 'updated', 'category')
