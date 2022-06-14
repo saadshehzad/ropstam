ee26868f0480ea2b22eeb8c794001d79436dd802
Muhammad Shehzad <xaadxhehzad@gmail.com>
2022-06-14 10:53:20 +0000

    Add urls for product CRUD

--- product/urls.py
+++ product/urls.py
@@ -8,5 +8,11 @@ urlpatterns = [
     path("create/category/", create_category, name="create_category"),
     path("categories/<int:id>/", get_category_by_id, name="get_category_by_id"),
     path("categories/<int:id>/update/", update_category, name="update_category"),
-    path("categories/<int:id>/delete/", delete_category, name="delete_category")
+    path("categories/<int:id>/delete/", delete_category, name="delete_category"),
+
+    path("products/", product_list, name="product_list"),
+    path("create/product/", create_product, name="create_product"),
+    path("products/<int:id>/", get_product_by_id, name="get_product_by_id"),
+    path("products/<int:id>/update/", update_product, name="update_product"),
+    path("products/<int:id>/delete/", delete_product, name="delete_product")
 ]
