7fe6acad824202e51602820badecc2c28a43832b
Muhammad Shehzad <xaadxhehzad@gmail.com>
2022-06-14 11:30:45 +0000

    Add django knox urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
     path("", include("product.urls")),
+    path('api/auth/', include('knox.urls'))
 ]
