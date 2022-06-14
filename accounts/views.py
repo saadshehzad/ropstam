3470d93d0dad7842b236e15b0435f6a4f7154b9c
Muhammad Shehzad <xaadxhehzad@gmail.com>
2022-06-14 11:05:56 +0000

    Update post method to send email

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views import generic
+from django.core.mail import send_mail
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +9,13 @@ class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "signup.html"
+
+    def post(self, request, *args, **kwargs):
+        send_mail(
+            'Thanks for registring',
+            'Here is the message.',
+            'ropstan@abc.com',
+            [request.user],
+            fail_silently=False,
+        )
+        return super().post(request, *args, **kwargs)
