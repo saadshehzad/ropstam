fd0df7fb66e8b9bc4b9b621176e991e996e6fd6d
Muhammad Shehzad <xaadxhehzad@gmail.com>
2022-06-14 11:18:51 +0000

    Add API Urls

--- product/urls.py
+++ product/urls.py
@@ -1,17 +1,25 @@
 from django.urls import path
+from api.views import *
 
 from .views import *
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    # Category URLS
     path("categories/", category_list, name="category_list"),
     path("create/category/", create_category, name="create_category"),
     path("categories/<int:id>/", get_category_by_id, name="get_category_by_id"),
     path("categories/<int:id>/update/", update_category, name="update_category"),
     path("categories/<int:id>/delete/", delete_category, name="delete_category"),
+    # Product URLS
     path("products/", product_list, name="product_list"),
     path("create/product/", create_product, name="create_product"),
     path("products/<int:id>/", get_product_by_id, name="get_product_by_id"),
     path("products/<int:id>/update/", update_product, name="update_product"),
     path("products/<int:id>/delete/", delete_product, name="delete_product"),
+    # API URLS
+    path("api/categories/", CategoryListView.as_view()),
+    path("api/categories/<int:pk>/", CategoryDetailView.as_view()),
+    path("api/products/", ProductListView.as_view()),
+    path("api/products/<int:pk>/", ProductDetailView.as_view()),
 ]
