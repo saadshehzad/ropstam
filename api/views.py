b77ef66b88e5513150fa3b316958a98dc4346007
Muhammad Shehzad <xaadxhehzad@gmail.com>
2022-06-14 11:30:04 +0000

    Add register api view and send email

--- api/views.py
+++ api/views.py
@@ -1,9 +1,26 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
 from django.contrib.auth import get_user_model
 from product.models import Category, Product
-from .serializers import CategorySerializer, ProductSerializer
+from .serializers import CategorySerializer, ProductSerializer, UserSerializer
+from rest_framework import permissions
+from django.core.mail import send_mail
 
 User = get_user_model()
+
+class CreateUserView(CreateAPIView):
+    model = get_user_model()
+    permission_classes = [permissions.AllowAny,]
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        send_mail(
+            'Thanks for registring',
+            'Here is the message.',
+            'ropstan@abc.com',
+            [request.user],
+            fail_silently=False,
+        )
+        return self.create(request, *args, **kwargs)
 
 
 class CategoryListView(ListCreateAPIView):
