e0d0ea9bd89ee6da2e6024b0ca811df8b3bfd926
Muhammad Shehzad <xaadxhehzad@gmail.com>
2022-06-14 08:40:40 +0000

    Add category and product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from .utils import TimeStampedModel
 
-# Create your models here.
+class Category(TimeStampedModel, models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(TimeStampedModel, models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
+    color = models.CharField(max_length=100)
+    model = models.IntegerField()
+    reg_no = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
