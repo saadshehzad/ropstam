0e0e058d8bcbde3167aff0a8f2f060f5ea104602
Muhammad Shehzad <xaadxhehzad@gmail.com>
2022-06-14 11:29:47 +0000

    Add user serializer

--- api/serializers.py
+++ api/serializers.py
@@ -14,3 +14,19 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+        return user
+
+    class Meta:
+        model = User
+        fields = ( "id", "username", "password", )
