e7064431cf50ccb78265cfc1e300761284abfe8c
Muhammad Shehzad <xaadxhehzad@gmail.com>
2022-06-14 10:36:59 +0000

    Add urls for category CRUD

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import *
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("categories/", category_list, name="category_list"),
+    path("create/category/", create_category, name="create_category"),
+    path("categories/<int:id>/", get_category_by_id, name="get_category_by_id"),
+    path("categories/<int:id>/update/", update_category, name="update_category"),
+    path("categories/<int:id>/delete/", delete_category, name="delete_category")
 ]
